40ae1b94b05ca9cc3aab68eb9006531d774fbbaf
Kevin Hobbs <kevin21921@gmail.com>
2018-08-16 14:18:45 -0400

    Added bootstrap sylings to post forms and added Quicksand google webfont as the default.

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,12 @@ require_relative 'boot'
 
 require 'rails/all'
 
+# Set Timezone for Database
+# Rails.application.configure do
+# 	config.time_zone = 'Eastern Time (US & Canada)'
+# 	config.active_record.default_timezone = :local
+# end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +16,8 @@ module Resortyoga
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.time_zone = 'Eastern Time (US & Canada)'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
+
+
+
 # Load the Rails application.
 require_relative 'application'
 
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
