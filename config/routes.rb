f233e5a746f30020f205cef6bbcb62d482114d7a
Kevin Hobbs <kevin21921@gmail.com>
2018-08-17 12:36:14 -0400

    Added about view, controller action and route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  
   root 'pages#home'
+  get '/about' => 'pages#about'
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
