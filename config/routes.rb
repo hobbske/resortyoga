f3048dd6d02a7c4606e110fc9e8f96825755786a
Kevin Hobbs <kevin21921@gmail.com>
2018-08-20 15:27:16 -0400

    Added contacts route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/about' => 'pages#about'
   resources :posts
+  resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
