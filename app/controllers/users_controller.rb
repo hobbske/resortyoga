67d732ed4a2cfd935fa3ad56e651f598395bb1ea
Kevin Hobbs <kevin21921@gmail.com>
2018-08-27 16:16:38 -0400

    Added user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
-class UsersController < ApplicationController
-	before_action :authenticate_user!
+# class UsersController < ApplicationController
+# 	before_action :authenticate_user!
 
 	
 
-end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+
   root 'pages#home'
   get '/about' 			=> 'pages#about'
   get '/isabella' 		=> 'pages#isabella'
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get '/products/betti_pot' => 'products#betti_pot'
   resources :posts
   resources :contacts
-  resources :products
+  resources :products  
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
